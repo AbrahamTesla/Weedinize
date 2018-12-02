83a5d9b4fc130ef284f61fbaa8869519b55b2417
Jordy <kieto@ualberta.ca>
2018-12-01 23:31:33 -0500

    fixed typo in version id

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = new Clarifai.App({
  apiKey: '7934b3f0139445e68b0ce91485e6c43e'
 });
 
-app.models.initModel({id: "Cannabis", version: "24b6f68b1a654372a6f17c2eab7e71f3npm"})
+app.models.initModel({id: "Cannabis", version: "24b6f68b1a654372a6f17c2eab7e71f3"})
       .then(generalModel => {
         return generalModel.predict("https://howtogrowmarijuana.com/wp-content/uploads/2015/12/Indica-Phenotype-Short-Fat-Leaves.jpg");
       })
