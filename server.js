befc411fbb17429cf12731608d1cf303210a12d5
AbrahamTesla <manlangitv@gmail.com>
2018-12-04 13:22:27 -0500

    fixed routes

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require("express");
 var app = express();
 var routes = require("./routes");
 var bodyParser = require('body-parser');
-var ENV = require("dotenv").config()
 
 
-app.use(bodyParser());
+
+app.use(bodyParser.json());// app.use(bodyParser());
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.text({type: 'json'}));
-app.use(bodyParser.json());
+
 
 app.use(express.static('./public'));
 
