3f69025f0c54baea0de3391fa2485ffef7fd4684
AbrahamTesla <manlangitv@gmail.com>
2018-12-04 13:49:47 -0500

    changes on server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 var app = express();
+
 var routes = require("./routes");
 var bodyParser = require('body-parser');
 
-
+//added this one
+var PORT = process.env.PORT || 8080
 
 app.use(bodyParser.json());// app.use(bodyParser());
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -14,6 +16,9 @@ app.use(express.static('./public'));
 
 app.use("/", routes);
 
-app.server = app.listen(8080, function(){
-  console.log('Weedinize on '+ app.server.address().port)
+// app.server = app.listen(8080, function(){
+//   console.log('Weedinize on '+ app.server.address().port)
+// });
+app.listen(PORT,function(){
+  console.log("App starting to PORT 8080: " + PORT)
 });
